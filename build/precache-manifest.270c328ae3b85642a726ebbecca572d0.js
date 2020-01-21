f997616a62b1c7f4f079db294bfad2600b72f545
Brandon Wie <it.brandonwie@gmail.com>
2020-01-21 12:43:23 +0400

    NOW doesn't work

---
+++ build/precache-manifest.270c328ae3b85642a726ebbecca572d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a81b43fc5b413c50d531f543a611b03",
+    "url": "/practice_React_Router/index.html"
+  },
+  {
+    "revision": "8b5d2bdb519fa72b1107",
+    "url": "/practice_React_Router/static/css/main.ceca84a1.chunk.css"
+  },
+  {
+    "revision": "75dac761c20420cebcc4",
+    "url": "/practice_React_Router/static/js/2.803b071b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/practice_React_Router/static/js/2.803b071b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b5d2bdb519fa72b1107",
+    "url": "/practice_React_Router/static/js/main.1778e9d8.chunk.js"
+  },
+  {
+    "revision": "7c9e34ac2dc7e97ce4b6",
+    "url": "/practice_React_Router/static/js/runtime-main.db9a898d.js"
+  }
+]);
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // App components
 import Header from "./Header";
@@ -11,7 +11,7 @@ import NotFound from "./NotFound";
 import Featured from "./Featured";
 
 const App = () => (
-  <HashRouter basename="/practice_React_Router">
+  <BrowserRouter>
     <div className="container">
       <Header />
       <Switch>
@@ -23,7 +23,7 @@ const App = () => (
         <Route component={NotFound} />
       </Switch>
     </div>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export default App;
